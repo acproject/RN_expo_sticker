4196766c413d7b26d75d9c9e0b694c3ca52d638f
acproject <acproject@qq.com>
2024-04-08 21:16:41 +0800

    update this project

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Platform } from 'react-native';
 import ImageViewer from './components/ImageViewer';
 import Button from './components/Button';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 
 import * as ImagePicker from 'expo-image-picker';
 import IconButton from './components/IconButton';
@@ -12,6 +12,12 @@ import EmojiPicker from './components/EmojiPicker';
 import EmojiList from './components/EmojiList';
 import EmojiSticker from './components/EmojiSticker';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+
+import * as MediaLibrary from 'expo-media-library';
+
+import { captureRef } from 'react-native-view-shot';
+import domtoimage from 'dom-to-image';
+
 
 const PlaceholderImage = require('./assets/images/background-image.png');
 
@@ -24,6 +30,46 @@ export default function App() {
 
   const [pickedEmoji, setPickedEmoji] = useState(null);
 
+  const [status, requestPermission] =  MediaLibrary.usePermissions();
+
+  const imageRef = useRef();
+
+  if (status === null) {
+    requestPermission();
+  }
+
+  const onSaveImageAsync = async () => {
+    if (Platform.OS !== 'web') {
+      try {
+        const localUri = await captureRef(imageRef, {
+          height: 440,
+          quality: 1,
+        });
+
+        await MediaLibrary.saveToLibraryAsync(localUri);
+        if (localUri) {
+          alert("Saved!");
+        }
+      } catch(e) {
+        console.log(e);
+      }
+    } else {
+      try {
+        const dataUrl = await domtoimage.toJpeg(imageRef.current, {
+          quality: 0.95,
+          width: 320,
+          height: 440,
+        });
+
+        let link = document.createElement('a');
+        link.download = 'sticker-smash.jpeg';
+        link.href = dataUrl;
+        link.click();
+      }catch (e) {
+        console.log(e);
+      }
+    }
+  };
 
   const onReset = () => {
     setShowAppOptions(false);
@@ -36,10 +82,6 @@ export default function App() {
   const onModalClose = () => {
     setIsModalVisible(false);
   }
-
-  const onSaveImageAsync = async () => {
-
-  };
 
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -58,13 +100,15 @@ export default function App() {
 
   return (
     <GestureHandlerRootView style={styles.container}>
-      <View style={styles.container}>
+        <View ref={imageRef} collapsable={false}>
         <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
           <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
         </EmojiPicker>
         <View style={styles.imageContainer}>
-          <ImageViewer placeholderImageSource={PlaceholderImage} selectedImage={selectedImage}/>
-          { pickedEmoji && <EmojiSticker imageSize={40} stickerSource={pickedEmoji} /> }
+          <View style={styles.container}>
+            <ImageViewer placeholderImageSource={PlaceholderImage} selectedImage={selectedImage}/>
+            { pickedEmoji && <EmojiSticker imageSize={40} stickerSource={pickedEmoji} /> }
+          </View>
         </View>
         {showAppOptions ? (
           <View style={styles.optionsContainer}>
@@ -80,7 +124,7 @@ export default function App() {
             <Button label="使用这张图片" onPress={() => setShowAppOptions(true)} />
           </View>
         )}
-        <StatusBar style="auto" />
+        <StatusBar style="light" />
       </View>
     </GestureHandlerRootView>
     
--- readme.md
+++ readme.md
@@ -1,3 +1,3 @@
 ### 测试新版的React Native
 
-本项目是一个测试新版本React-Native项目，该项目来至[Expo](https://docs.expo.dev/turorial/gestures)的教程。主要目的是用于测试新版的React-Native。
+本项目是一个测试新版本React-Native项目，该项目来至[Expo](https://docs.expo.dev)的教程。主要目的是用于测试新版的React-Native。
